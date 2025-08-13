e0b56c1c4cd7307ecc0d2e0f89f127b59e58c697
Jrfr <161663111+JafFR13@users.noreply.github.com>
2025-08-13 12:19:36 +0700

    Add files via upload

---
+++ kasir-12-rpl/src/kasir/dbkoneksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kasir;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author user
+ */
+public class dbkoneksi {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/kasirDB";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345678";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    
+}
+
+}
